d78db7ebc00ff707705e0d1396d161adbf8d633c
Peter <1301peter@gmail.com>
2013-11-20 11:40:33 +0100

    Añadir master 2

--- JavaApplication14/src/javaapplication14/JavaApplication14.java
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -18,7 +18,7 @@ public class JavaApplication14 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int a,b;
+        int a,b,c;
     }
     
 }
